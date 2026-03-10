e1fe3d9c2680f0967650cff809a8ccbb1d5db5a2
Karol Binkowski <karolbinkowski3@proton.me>
2026-03-10 14:18:11 +0100

    feat: implement example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,20 +1,74 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { multiply } from 'react-native-screenshot-guard';
-
-const result = multiply(3, 7);
+import { useScreenshotGuard } from 'react-native-screenshot-guard';
+import { useState } from 'react';
+import { Pressable, ScrollView, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+  const [isProtected, setIsProtected] = useState(true);
+
+  useScreenshotGuard(isProtected);
+
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.container}>
+          <Text style={styles.header}>expo-screenshot-guard</Text>
+          <View style={styles.group}>
+            <Text style={styles.groupHeader}>Screenshot Protection</Text>
+            <Text>
+              Protection is currently:{' '}
+              <Text style={styles.bold}>
+                {isProtected ? 'ENABLED' : 'DISABLED'}
+              </Text>
+            </Text>
+            <Pressable
+              style={styles.button}
+              onPress={() => setIsProtected((prev) => !prev)}
+            >
+              <Text style={styles.buttonText}>
+                {isProtected ? 'Disable Protection' : 'Enable Protection'}
+              </Text>
+            </Pressable>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = {
+  header: {
+    fontSize: 30,
+    margin: 20,
+  },
+  groupHeader: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  bold: {
+    fontWeight: 'bold' as const,
+  },
+  group: {
+    margin: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 20,
+    gap: 16,
+  },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#eee',
   },
-});
+  button: {
+    backgroundColor: '#007AFF',
+    borderRadius: 8,
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    alignItems: 'center' as const,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold' as const,
+  },
+};
